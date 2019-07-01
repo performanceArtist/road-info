f3e215d69837affb9ec0dc770f218645a3c46076
performanceArtist1 <nakedant19@gmail.com>
2019-07-01 12:12:03 +0600

    Path fix

--- client/src/app/components/InfoModal/InfoModal.tsx
+++ client/src/app/components/InfoModal/InfoModal.tsx
@@ -4,7 +4,8 @@ import uuid from 'short-uuid';
 import { connect } from 'react-redux';
 
 import Modal from '../Modal/Modal';
-import { closeModal } from '../../redux/modal/actions';
+//import { closeModal } from '@redux/modal/actions';
+import { closeModal } from '@redux/modal/actions';
 
 interface Notice {
   message: string;
